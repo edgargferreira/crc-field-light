9fc787f78b6285c2071b3815f7fbc250c0c3b56b
Edgar Ferreira <eferreira@imaginarycloud.com>
2024-08-09 14:56:36 +0100

    First commit

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+(() => {
+  // src/main.js
+  $(() => {
+    $("#header_section").load("src/header.html");
+    $("#field_section").load("src/field.html");
+  });
+})();
---
+++ src/main.js
@@ -0,0 +1,4 @@
+$(() => {
+  $('#header_section').load('src/header.html');
+  $('#field_section').load('src/field.html');
+});
